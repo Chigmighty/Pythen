4f6c26ec1a851b806e9e88459b75cc6c73d63e79
Chig <salient.moment@outlook.com>
2022-08-17 20:17:16 -0600

    alpha

---
+++ main.py
@@ -0,0 +1,11 @@
+calculation_of_units = 24*60*60
+name_of_unit = 'seconds'
+
+print(f"20 days {calculation_of_units} {name_of_unit}")
+
+first_name = 'Mercy'
+surname = 'Solomon'
+
+print("my first name is {}. My surname is {}".format(first_name,surname))
+
+t se
